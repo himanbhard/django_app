1f6c44d683e2614220b7fa4b1995cf847ac27c3d
himanshubhardwaj <himanbhard@gmail.com>
2020-05-21 14:15:06 -0400

    init commit

---
+++ meeting_planner/website/views.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+
+from django.shortcuts import render
+from django.http import HttpResponse
+from meetings.models import Meeting
+
+def welcome(request):
+    return render(request, "website/welcome.html",
+                    {"meetings":Meeting.objects.all()})
+
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+
+# Please add: An about page that shows some text about yourself
+def about(request):
+    return HttpResponse("I'm Himanshu and I solve problems for a living.")
+
+def home(request):
+    return render(request, "website/welcome.html")
